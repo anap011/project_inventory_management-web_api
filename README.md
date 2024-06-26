172619ed7d763601c091d3428f0ef21af095b87a
Administrador <Administrador@DESKTOP-ANA>
2024-06-26 20:11:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## CRUD User Frontend
+
+Este respoitorio contiene el servicio Frontend que se comunica con una API-REST.
+El proyecto consta de otro servicio interconectado que realiza la función del Backend.
+Este servicio se encuentra en el repositorio "CRUDUserBackend" 
+
+# Contenido
+La Aplicación Web en React realiza consultas HTTP GET a la API-REST para luego 
+mostrar los datos en una interfaz visual para el usuario. 
---
+++ src/App/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { Nav } from 'rsuite';
+
+const App = () => {
+    const [data, setData] = useState([]);
+    const [activeKey, setActiveKey] = useState("product");
+
+    const fetchData = (endpoint) => {
+        fetch(`https://www.api-inventory-management.somee.com/${endpoint}`, {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((jsonData) => {
+                setData(jsonData); // Asigna los datos recibidos al estado 'data'
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
+
+    useEffect(() => {
+        fetchData('product');
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h2>
+                    Bienvenid@ a API Inventory Management
+                </h2>
+            </header>
+            <section className='App-description'>
+                <p>
+                    Esta página web que he desarrollado utilizando React imprime en formato JSON los datos que obtiene de la conexión a una API-REST pública para métodos GET propia, la cual utiliza C# .NET.
+                </p>
+                <p>En el caso de que se quiera probar la API:
+                    <br />
+                    - Productos (Products): <a href="http://www.api-inventory-management.somee.com/product" target="_blank" rel="noopener noreferrer">http://www.api-inventory-management.somee.com/product</a>
+                    <br />
+                    - Proveedores (Suppliers): <a href="http://www.api-inventory-management.somee.com/supplier" target="_blank" rel="noopener noreferrer">http://www.api-inventory-management.somee.com/supplier</a>
+                    <br />
+                    - Lotes (Batches): <a href="http://www.api-inventory-management.somee.com/batch" target="_blank" rel="noopener noreferrer">http://www.api-inventory-management.somee.com/batch</a>
+                    <br />
+                    <br />
+                    Si se quiere filtrar por "id": <a href="http://www.api-inventory-management.somee.com/tabla/id=NUMERO_ID" target="_blank" rel="noopener noreferrer">http://www.api-inventory-management.somee.com/"NOMBREDETABLA"/id="NUMERO_ID"</a>
+                </p>
+            </section>
+
+            <section className='App-nav'>
+                <Nav justified appearance='default' ActiveKey={activeKey} onSelect={setActiveKey}>
+                    <Nav.Item eventKey="Productos" onSelect={() => fetchData('product')} className="nav-item"> Productos (Products)</Nav.Item>
+                    <Nav.Item eventKey="Proveedores" onSelect={() => fetchData('supplier')} className="nav-item">Proveedores (Suppliers)</Nav.Item>
+                    <Nav.Item eventKey="Lotes" onSelect={() => fetchData('batch')} className="nav-item">Lotes (Batches)</Nav.Item>
+                    {/*<Nav.Item eventKey="About" className="nav-item">About</Nav.Item> */}
+                </Nav>
+            </section>
+            
+            <div className='App-content'>
+                <div className='App-json'>
+                    <pre>{JSON.stringify(data, null, 5)}</pre>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
